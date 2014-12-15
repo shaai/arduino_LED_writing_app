200e1de6dfb70bc836accc01af279edb3417ec84
shaai <shaai.sattar@gmail.com>
2014-12-15 17:20:08 -0500

    code added so that a user now shows up when they connect to a server

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
